be75bc354ee7b69f6c4959bb9e8e2b82d6cee487
dcherevatenko <denis.cherevatenko@peopleforce.io>
2023-04-19 10:00:52 +0300

    Updates after code review

--- app/admin/inputs/numeric_range_Input.rb
+++ app/admin/inputs/numeric_range_Input.rb
@@ -3,16 +3,6 @@ module ActiveAdmin
     class NumericRangeInput < ::Formtastic::Inputs::StringInput
       include Filters::Base
 
-      # def to_html
-      #   input_wrapping do
-      #     [ label_html,
-      #       builder.text_field(gt_input_name, input_html_options(gt_input_name)),
-      #       template.content_tag(:span, "-", :class => "seperator"),
-      #       builder.text_field(lt_input_name, input_html_options(lt_input_name)),
-      #     ].join("\n").html_safe
-      #   end
-      # end
-
       def to_html
         input_wrapping do
           [ label_html,
